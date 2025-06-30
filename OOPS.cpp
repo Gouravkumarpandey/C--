617121a22f8bf61887cdef85cd5c3a1b63672162
Gouravkumarpandey <kumarpandeygourav@gmail.com>
2025-06-30 16:55:00 +0530

    add

--- OOPS.cpp
+++ OOPS.cpp
@@ -3,17 +3,29 @@
 using namespace std;
 
 class Teacher{
+    private:     
+    double salary;
+
+   public:
     //properties // attributes
     string  name ;
     string  dept;
     string subject ;
-    double salary;
 
     //methods // member functions
 
     void changeDept(string newDept){
         dept= newDept;
     }
+    //setter
+        void setSalary(double s){
+            salary = s;
+        }
+        //getter
+            double getSalary(){
+                return salary;
+            }
+    
 
 };
 
@@ -21,7 +33,7 @@ int main(){
   Teacher t1;
     t1.name = "Gourav";
     t1.dept ="Computer Science"
-    t1.subject = "C++ Programming";
+    t1.subject ="C++ Programming";
     t1.salary = 50000;
   
 
